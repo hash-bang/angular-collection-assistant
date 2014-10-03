2129974f95a4da19d75c6cb4b1c81e03157f88a7
Matt Carter <m@ttcarter.com>
2014-10-03 14:03:12 +1000

    Added all inline documentation Added dirty detection which avoids the need to call 'calculate' explicitally before 'on'

--- ng-collection-assistant.js
+++ ng-collection-assistant.js
@@ -7,39 +7,79 @@ angular.module('ng-collection-assistant', [])
 			this._deleted = [];
 			this._same = [];
 			this._updated = [];
-			this._indexBy = null;
+			this._indexBy = 'id';
 			this._deepExamine = false;
+			this._dirty = true;
 
-			console.log('EXAMINE', newCollection, oldCollection);
-
+			/**
+			* Set the key used to index each item in a collection
+			* Defaults to 'id'
+			* @param string index The new key to use for indexing
+			* @return object This chainable object
+			*/
 			this.indexBy = function(index) {
 				this._indexBy = index;
-				this.calculate();
+				this._dirty = true;
 				return this;
 			};
 
+			/**
+			* Internal function used to return the index of an object
+			* @param object item The item to examine and return the index of
+			* @return string The index of the item
+			*/
 			this.getIndexOf = function(item) {
 				return item[this._indexBy];
 			};
 
+			/**
+			* Setter for getIndexOf
+			* @param function(item) callback Replacement function which indexes items in a collection
+			* @return object This chainable object
+			*/
 			this.indexer = function(callback) {
 				this.getIndexOf = callback;
+				this._dirty = true;
 				return this;
 			};
 
+			/**
+			* Update detection function if deepComparison is true
+			* @param object a The object on the left side to compare
+			* @param object b The object on the right side to compare
+			* @return bool True if the objects are deep equal
+			*/
 			this.getUpdates = function(a, b) {
 				return JSON.stringify(a) === JSON.stringify(b);
 			};
 
+			/**
+			* Set whether deep comparison should be used
+			* Enabling this will enable update detectin
+			* @param bool setting Whether deep comparison is true. Default to true if omitted
+			* @return object This chainable object
+			*/
 			this.deepComparison = function(setting) {
-				this._deepExamine = setting;
+				this._deepExamine = setting || true;
+				this._dirty = true;
 				return this;
 			};
 
+			/**
+			* Setter for the deep comparison function
+			* @param callback(a, b) callback The callback to use when deep comparing objects a and b
+			* @return object This chainable object
+			*/
 			this.updateDetector = function(callback) {
 				this.getUpdates = callback;
+				this._dirty = true;
+				return this;
 			};
 
+			/**
+			* Internal function which calculates all parameters
+			* @return object This chainable object
+			*/
 			this.calculate = function() {
 				this._created = [];
 				this._deleted = [];
@@ -77,6 +117,11 @@ angular.module('ng-collection-assistant', [])
 				return this;
 			};
 
+			/**
+			* Chainable function which runs when collection items are created, deleted etc.
+			* @param string type The type of operation to look for. See code for examples.
+			* @return object This chainable object
+			*/
 			this.on = function(type, callback) {
 				switch (type) {
 					case 'new':
@@ -96,27 +141,56 @@ angular.module('ng-collection-assistant', [])
 				}
 			};
 
+			/**
+			* Function to run on each new item discovered in a collection set
+			* @param callback(item) callback The callback to execute on each new item
+			* @return object This chainable object
+			* @see On()
+			*/
 			this.newItem = function(callback) {
+				if (this._dirty) this.calculate();
 				for (var offset in this._created) {
 					callback(_newPointers[this._created[offset]]);
 				}
 			};
 
+			/**
+			* Function to run on each item discovered to be deleted in a collection set
+			* @param callback(item) callback The callback to execute on each deleted item
+			* @return object This chainable object
+			* @see On()
+			*/
 			this.deleteItem = function(callback) {
+				if (this._dirty) this.calculate();
 				for (var offset in this._deleted) {
 					callback(_oldPointers[this._deleted[offset]]);
 				}
 			};
 
+			/**
+			* Function to run on each item discovered to be unchanged in a collection set
+			* @param callback(item) callback The callback to execute on each unchanged item
+			* @return object This chainable object
+			* @see On()
+			*/
 			this.sameItem = function(callback) {
+				if (this._dirty) this.calculate();
 				for (var offset in this._same) {
 					callback(_oldPointers[this._same[offset]]);
 				}
 			};
 
+			/**
+			* Function to run on each updated item collection set
+			* This function can only run if DeepComparison() is called first
+			* @param callback(item, oldItem) callback The callback to execute on each changed item
+			* @return object This chainable object
+			* @see On()
+			*/
 			this.updateItem = function(callback) {
+				if (this._dirty) this.calculate();
 				for (var offset in this._updated) {
-					callback(_newPointers[this._updated[offset]]);
+					callback(_newPointers[this._updated[offset]], _oldPointers[this._updated[offset]]);
 				}
 			};
 
